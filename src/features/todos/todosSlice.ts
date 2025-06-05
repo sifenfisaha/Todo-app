190ebc34552dbf9f449b5d947cde99f22d5a331d
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-06-05 18:28:42 +0300

    feat: setup Redux Toolkit with todos slice, typed store, and state management

---
+++ src/features/todos/todosSlice.ts
@@ -0,0 +1,64 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { TodosState, Todo, Filter } from "../../types/types";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { nanoid } from "@reduxjs/toolkit";
+
+const initialState: TodosState = {
+  todos: [],
+  filter: "all",
+  loading: false,
+  error: null,
+};
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: {
+      reducer: (state, action: PayloadAction<Todo>) => {
+        state.todos.push(action.payload);
+      },
+      prepare: (text: string) => {
+        return {
+          payload: { id: nanoid(), text, completed: false } as Todo,
+        };
+      },
+    },
+    toggleTodo: (state, action: PayloadAction<String>) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) todo.completed = !todo.completed;
+    },
+    editTodo: (state, action: PayloadAction<{ id: string; text: string }>) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload.id);
+      if (todo) todo.text = action.payload.text;
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    setFilter: (state, action: PayloadAction<Filter>) => {
+      state.filter = action.payload;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action: PayloadAction<string | null>) => {
+      state.error = action.payload;
+    },
+    loadTodos: (state, action: PayloadAction<Todo[]>) => {
+      state.todos = action.payload;
+    },
+  },
+});
+
+export const {
+  addTodo,
+  toggleTodo,
+  editTodo,
+  deleteTodo,
+  setFilter,
+  setLoading,
+  setError,
+  loadTodos,
+} = todosSlice.actions;
+
+export default todosSlice.reducer;
---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+};
+
+export type Filter = "all" | "active" | "completed";
+
+export type TodosState = {
+  todos: Todo[];
+  filter: Filter;
+  loading: boolean;
+  error: string | null;
+};
