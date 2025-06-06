17e26c36ffe67c11d1fcdea585b10606654227b1
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-06-06 21:24:40 +0300

    fix: use absolute paths for background images to display correctly on Netlify

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Background: React.FC = () => {
   return (
     <>
-      <div className="z-[-100] bg-[url('bg-desktop-light.jpg')] dark:bg-[url('bg-desktop-dark.jpg')] hidden md:block absolute top-0  bg-cover bg-center  w-full h-[40%]"></div>
-      <div className="z-[-100] bg-[url('bg-mobile-light.jpg')] dark:bg-[url('bg-mobile-dark.jpg')]  h-[40%]  md:hidden absolute top-0  bg-cover bg-center w-full"></div>
+      <div className="z-[-100] bg-[url('/bg-desktop-light.jpg')] dark:bg-[url('/bg-desktop-dark.jpg')] hidden md:block absolute top-0 bg-cover bg-center w-full h-[40%]"></div>
+      <div className="z-[-100] bg-[url('/bg-mobile-light.jpg')] dark:bg-[url('/bg-mobile-dark.jpg')] h-[40%] md:hidden absolute top-0 bg-cover bg-center w-full"></div>
     </>
   );
 };
